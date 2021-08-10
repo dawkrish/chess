d9ce07654c8cf391f504f6e784f554b920484258
krishnanshagarwal112 <gonkillua2680@gmail.com>
2021-08-10 23:40:46 +0530

    game window under construction

--- code/gui/auth.py
+++ code/gui/auth.py
@@ -1,139 +1,38 @@
 from .config import *
+from .signin import *
+from .signup import *
 from tkinter import *
 from ..database.get import get
 from ..database.create import create_user
+
 
 main_window=Tk()
 main_window.geometry(f"{AUTH_WIDTH}x{AUTH_HEIGHT}") # window dimensions
 main_window.title(f"{TITLE}")            # window title
 main_window.config(background = "White")
 
+photo = PhotoImage(file="chess.png")
+photo_label = Label(main_window, image=photo)
+photo_label.place(x=0,y=0)
+
 heading = Label(main_window,
     text = "Welcome to Chess",
-    font = ("Alef", 28, "bold"),
-    fg = "Green",
-    bg = "White")    
+    font = ("Alef", 35, "bold"),
+    fg = "green",
+    bg="white")
+
 heading.pack()
-
-
-def main_signin():
-    window = Tk()
-    window.geometry(f"{AUTH_WIDTH}x{AUTH_HEIGHT}") # window dimensions
-    window.title(f"{TITLE}")            # window title
-    window.config(background = "White") # window color
-
-    form_heading = Label(window,
-        text="Sign In",
-        font=("Arial", 30),
-        fg="Black",
-        bg="White")
-    form_heading.pack()
-
-    username_label = Label(window,
-        text = "Enter your username",
-        font = ("Arial",20),
-        bg = "White")
-    username_label.pack()
-
-    username_entry = Entry(window,
-        font = (("arial", 20)))
-    username_entry.pack()
-
-    password_label = Label(window,
-        text = "Enter your password!",
-        font = ("Arial",20),
-        bg = "White")
-    password_label.pack()
-
-    password_entry = Entry(window,
-        font = ("arial",20),
-        show = "*")
-    password_entry.pack()
-
-    def signin_fn():
-        username = username_entry.get()
-        password = password_entry.get()
-        res = get("users", f"username = '{username}'")
-        if len(res) != 1:
-            raise ValueError("Invalid User")
-        user_details = res[0]
-        if user_details[1] != password:
-            raise ValueError("Invalid Password")
-        print("Done")
-        window.destroy()
-
-    signin_button = Button(window,
-        text = "Submit",
-        command = signin_fn)
-
-    signin_button.pack()
-
-    window.mainloop()
-
-def main_signup():
-    window = Tk()
-    window.geometry(f"{AUTH_WIDTH}x{AUTH_HEIGHT}") # window dimensions
-    window.title(f"{TITLE}")            # window title
-    window.config(background = "White") # window color
-
-    form_heading = Label(window,
-        text="Sign Up",
-        font=("Arial", 30),
-        fg="Black",
-        bg="White")
-    form_heading.pack()
-
-    username_label = Label(window,
-        text = "Enter your username",
-        font = ("Arial",20),
-        bg = "White")
-    username_label.pack()
-
-    username_entry = Entry(window,
-        font = (("arial", 20)))
-    username_entry.pack()
-
-    password_label = Label(window,
-        text = "Enter your password!",
-        font = ("Arial",20),
-        bg = "White")
-    password_label.pack()
-
-    password_entry = Entry(window,
-        font = ("arial",20),
-        show = "*")
-    password_entry.pack()
-
-    def signup_fn():
-        username = username_entry.get()
-        res = get("users", f"username = '{username}'")
-        if len(res) != 0:
-            raise ValueError("User already exists")
-        password = password_entry.get()
-        if len(password) < 8:
-            raise ValueError("Password is too short (should be >= 8)!")
-        create_user(username, password)
-        print("Done")
-        window.destroy()
-
-    signup_button = Button(window,
-        text = "Submit",
-        command = signup_fn)
-
-    signup_button.pack()
-
-    window.mainloop()
-
 
 sign_in_button = Button(main_window,
     text = "Sign In",
-    font = ("Arial",30),
-    command = main_signin)
+    font = ("Arial",12),
+    command = main_signin,
+    padx=5.3)
 sign_in_button.pack()
 
 sign_up_button = Button(main_window,
     text = "Sign Up",
-    font = ("Arial",30),
+    font = ("Arial",12),
     command = main_signup)
 sign_up_button.pack()
 
--- code/gui/game.py
+++ code/gui/game.py
@@ -1,1 +1,40 @@
-# Main game window
+from tkinter import *
+from ..classes.vars import position_color_relation
+from .config import *
+
+game_window = Tk()
+game_window.geometry(f"{GAME_WIDTH}x{GAME_HEIGHT}")
+game_window.grid_columnconfigure(0, weight=1)
+
+'''heading = Label(game_window,
+                fg = "White",
+                bg = "Black")
+heading.grid(row=0, column=0)'''
+
+
+
+for row in range(8, 0, -1):
+    new_row_frame = Frame(game_window, width=GAME_WIDTH, height=GAME_HEIGHT // 8)
+    new_row_frame.grid(row = 8 - row + 1, column = 0, pady=20)
+    new_row_frame.grid_propagate(False)
+    for column in range(97, 105):
+        position_name = f'{chr(column)}{row}'
+        new_tile = Button(new_row_frame, bg = position_color_relation[position_name], width = GAME_WIDTH // 8, height = GAME_HEIGHT // 8)
+        new_row_frame.grid_columnconfigure(column - 97, weight=0)
+        new_tile.grid(row=0,column=column-97)
+
+#Button(game_window, text="Button-1",height= 5, width=10).pack()
+
+'''_
+chess engine
+
+
+tile - tile - tile - tile --- tile
+|       
+|
+|
+tile - tile - tile - tile --- tile
+
+'''
+
+game_window.mainloop()
